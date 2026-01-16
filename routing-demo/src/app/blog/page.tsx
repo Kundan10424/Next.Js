df978b508fda116095b33176a1a1b2e0bad81f74
Kundan10424 <kundankumar10424>
2026-01-16 16:32:41 +0530

    Loader

--- routing-demo/src/app/blog/page.tsx
+++ routing-demo/src/app/blog/page.tsx
@@ -1,3 +1,19 @@
-export default function Blog() {
+import { Metadata } from "next";
+
+export const metadata: Metadata ={
+  title:{
+    absolute: "Blog Page"
+  }
+}
+
+
+export default async function Blog() {
+
+  await new Promise((res) => {
+    setTimeout(() => {
+      res("Intentional Delay")
+    }, 2000)
+  })
+
   return <h1>Welcome to the Blog Page</h1>;
 }
