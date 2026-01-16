9401451b42d61074d629e0c7ca3da2614364249d
Kundan10424 <kundankumar10424>
2026-01-16 15:57:09 +0530

    params and search params

--- routing-demo/src/app/page.tsx
+++ routing-demo/src/app/page.tsx
@@ -7,6 +7,9 @@ export default function Home(){
             <h1>Welcome to the Home Page</h1>
             <Link href="/blog">Blog</Link>
             <Link href="/products">Products</Link>
+            <Link href="/articles/breaking-news-123?lang=en"> Read in English</Link>
+            <Link href="/articles/breaking-news-123?lang=es"> Read in Spanish</Link>
+            <Link href="/articles/breaking-news-123?lang=fr"> Read in French</Link>
         </>
     )
 }
