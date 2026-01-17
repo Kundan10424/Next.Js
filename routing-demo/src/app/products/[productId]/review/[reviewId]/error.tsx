cbf753d4ae2315d29f9240aef0cf61901b7ab2ba
Kundan10424 <kundankumar10424>
2026-01-17 15:25:27 +0530

    reload & reset

--- routing-demo/src/app/products/[productId]/review/[reviewId]/error.tsx
+++ routing-demo/src/app/products/[productId]/review/[reviewId]/error.tsx
@@ -3,8 +3,30 @@
 
 "use client"
 
-export default function ErrorBoundary({error}:{error: Error}){
+import {useRouter} from "next/navigation"
+import {startTransition} from "react"
+
+export default function ErrorBoundary(
+    {
+        error,
+        reset,
+    }:{
+        error: Error;
+        reset: () => void
+    }){
+
+        const router = useRouter()
+        const reload = () =>{
+            startTransition(() => {
+                router.refresh()
+                reset()
+            })
+        }
+
     return(
-        <h1>{error.message}</h1>
+        <div>
+            <p>{error.message}</p>
+            <button onClick={reload}>Try again</button>
+        </div>
     )
 }
