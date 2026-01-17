d310f8fcf44a0c8b92bfcf3599d65802cf4d69c4
Kundan10424 <kundankumar10424>
2026-01-17 15:12:39 +0530

    Error handling

---
+++ routing-demo/src/app/products/[productId]/review/[reviewId]/page.tsx
@@ -0,0 +1,28 @@
+import { notFound } from "next/navigation";
+
+
+function getRandomInt(count: number){
+    return Math.floor(Math.random() * count);
+}
+
+export default async function ProductReview({
+    params,
+}:{
+    params: Promise<{productId: string; reviewId: string;}>
+}){
+    const random = getRandomInt(2);
+    if(random === 1){
+        throw new Error('Random error occurred');
+    }
+    const {productId, reviewId} = await params;
+
+    if(parseInt(reviewId)>1000){
+        notFound();
+    }
+
+    return(
+        <h1>
+            Review {reviewId} for Product {productId}
+        </h1>
+    )
+}
