12ff750340d4a02ba70161b559933acd50150e98
Kundan10424 <kundankumar10424>
2026-01-19 16:25:38 +0530

    parallel routes

---
+++ routing-demo/src/app/dashboard/@notifications/page.tsx
@@ -0,0 +1,5 @@
+import { Card } from "@/src/components/card";
+
+export default function Notifications() {
+  return <Card>Welcome to the Notifications Page</Card>;
+}
---
+++ routing-demo/src/app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+export default function DashboardPage(){
+    return(
+        <h1 className="font-bold text-2xl">DashBoard</h1>
+    )
+}
