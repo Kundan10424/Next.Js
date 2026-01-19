c1ea7404241bd6c2295d685cd14b5a6cc9e2e028
Kundan10424 <kundankumar10424>
2026-01-19 17:02:13 +0530

    unmatched routes

--- routing-demo/src/app/dashboard/@notifications/page.tsx
+++ routing-demo/src/app/dashboard/@notifications/page.tsx
@@ -1,5 +1,13 @@
 import { Card } from "@/src/components/card";
+import Link from "next/link";
 
 export default function Notifications() {
-  return <Card>Welcome to the Notifications Page</Card>;
+  return (
+    <Card>
+      <div>Notifications</div>
+      <div>
+        <Link className="underline decoration-blue-500 text-blue-500" href="/dashboard/archived">Archived</Link>
+      </div>
+    </Card>
+  );
 }
