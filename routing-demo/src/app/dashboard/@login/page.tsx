ea7fa5395649810a4e4f95e4be863eaeb1c94df7
Kundan10424 <kundankumar10424>
2026-01-21 14:14:36 +0530

    conditional routing

---
+++ routing-demo/src/app/dashboard/@login/page.tsx
@@ -0,0 +1,11 @@
+
+
+
+
+import {Card} from "@/src/components/card"
+
+export default function LoginSlot(){
+    return(
+        <Card>Please login to continue</Card>
+    )
+}
--- routing-demo/src/app/dashboard/layout.tsx
+++ routing-demo/src/app/dashboard/layout.tsx
@@ -3,14 +3,18 @@ export default function DashboardLayout({
     children,
     users,
     revenue,
-    notifications
+    notifications,
+    login,
 }:{
     children: React.ReactNode;
     users: React.ReactNode;
     revenue: React.ReactNode;
     notifications: React.ReactNode;
+    login: React.ReactNode;
+
 }){
-    return(
+    const isLoggedIn = true;
+    return isLoggedIn? (
         <div>
             <div>{children}</div>
             <div style={{display:"flex"}}>
@@ -21,5 +25,7 @@ export default function DashboardLayout({
                 <div style={{display: "flex", flex:1}}>{notifications}</div>
             </div>
         </div>
+    ):(
+        <div>{login}</div>
     )
 }
