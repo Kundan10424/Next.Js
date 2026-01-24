a8343d54f60687607891c1da079bb971b697a67d
Kundan10424 <kundankumar10424>
2026-01-24 15:21:29 +0530

    handling PATCH request

--- route-handlers/src/app/comments/[id]/route.ts
+++ route-handlers/src/app/comments/[id]/route.ts
@@ -8,3 +8,18 @@ export async function GET(_req: Request, {params}: {params: Promise<{id: string}
     const comment = comments.find(comment => comment.id === parseInt(id))
     return Response.json(comment)
 }
+
+export async function PATCH(req: Request, {params}: {params: Promise<{id: string}>}){
+    const {id} = await params
+    const body = await req.json()
+    const {text} = body
+
+    const commentIndex = comments.findIndex(comment => comment.id === parseInt(id))
+
+    if(commentIndex === -1){
+        return Response.json({message: "Comment not found"}, {status: 404})
+    }
+    comments[commentIndex].text = text;
+
+    return Response.json(comments[commentIndex])
+}
