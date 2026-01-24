3360c38530a2255cad755cda2a8cb7c3340bfd11
Kundan10424 <kundankumar10424>
2026-01-24 15:30:44 +0530

    handling DELETE request

--- route-handlers/src/app/comments/[id]/route.ts
+++ route-handlers/src/app/comments/[id]/route.ts
@@ -23,3 +23,18 @@ export async function PATCH(req: Request, {params}: {params: Promise<{id: string
 
     return Response.json(comments[commentIndex])
 }
+
+export async function DELETE(_req: Request, {params}: {params: Promise<{id: string}>}){
+    const {id} = await params
+    const commentIndex = comments.findIndex(comment => comment.id === parseInt(id))
+
+    const deletedComment = comments[commentIndex]
+
+    if(commentIndex === -1){
+        return Response.json({message: "Comment not found"}, {status: 404})
+    }
+
+    comments.splice(commentIndex, 1)
+
+    return Response.json({message: "Comment deleted successfully", deletedComment})
+}
