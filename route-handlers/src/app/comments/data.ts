50eb9ab9a1f63f5e9d9d2d6b5a6b1b16df3e1c56
Kundan10424 <kundankumar10424>
2026-01-24 14:36:52 +0530

    handling GET request

---
+++ route-handlers/src/app/comments/data.ts
@@ -0,0 +1,17 @@
+
+
+
+export const comments = [
+    {
+        id:1,
+        text: "Great post! Really enjoyed reading it.",
+    },
+    {
+        id:2,
+        text: "I disagree with some points, but overall a good read.",
+    },
+    {
+        id:3,
+        text: "Can you provide more details on the topic?",
+    },
+];
