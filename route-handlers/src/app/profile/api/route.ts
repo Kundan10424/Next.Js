f3a4f26de388bc793be73206ec184da0f12d4c80
Kundan10424 <kundankumar10424>
2026-01-24 15:56:43 +0530

    headers in route handlers

--- route-handlers/src/app/profile/api/route.ts
+++ route-handlers/src/app/profile/api/route.ts
@@ -1,3 +1,12 @@
-export async function GET(){
+import { NextRequest } from "next/server";
+import { headers } from "next/headers";
+
+
+export async function GET(req: NextRequest){
+    // const requestHeaders = new Headers(req.headers);
+    // console.log("Authorization:", requestHeaders.get("authorization"));
+
+    const allHeaders = await headers();
+    console.log("Headers list", allHeaders.get("authorization"));
     return new Response("Profile API data");
 }
