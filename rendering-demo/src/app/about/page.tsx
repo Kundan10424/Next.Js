4b1dc8851fb9169d1a112350c70e00b149e044fb
Kundan10424 <kundankumar10424>
2026-02-03 15:05:24 +0530

    dynamic rendering

--- rendering-demo/src/app/about/page.tsx
+++ rendering-demo/src/app/about/page.tsx
@@ -1,4 +1,9 @@
-export default function About(){
+import {cookies} from 'next/headers';
+
+export default async function About(){
+    const cookieStore = await cookies();
+    const theme = cookieStore.get('theme')
+    console.log('Theme cookie:', theme);
     console.log("Rendering About Page");
     return <h1>About Page {new Date().toLocaleTimeString()}</h1>
 }
