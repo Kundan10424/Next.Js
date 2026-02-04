61863b08c3a361b7475e9af00be60adfe0801756
Kundan10424 <kundankumar10424>
2026-02-04 15:11:53 +0530

    third-party-packages

--- rendering-demo/src/app/server-route/page.tsx
+++ rendering-demo/src/app/server-route/page.tsx
@@ -1,6 +1,14 @@
+import { ImageSlider } from "@/src/components/imageSlider";
 import { serverSideFunction } from "@/src/utils/server-utils";
 
 export default function ServerRoute(){
     const result= serverSideFunction();
-    return <h1>Server Route Page: {result}</h1>
+    return (
+        <>
+         <h1>Server Route Page: {result}</h1>
+        <ImageSlider />
+        </>
+   
+)
+   
 }
--- rendering-demo/src/components/imageSlider.tsx
+++ rendering-demo/src/components/imageSlider.tsx
@@ -1,13 +1,11 @@
 "use client"
 
-// import { serverSideFunction } from "@/src/utils/server-utils";
-
 import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
-export default function ClientRoute() {
+export const ImageSlider = () => {
   const settings = {
     dots: true,
   };
@@ -15,16 +13,16 @@ export default function ClientRoute() {
     <div className="image-slider-container">
       <Slider {...settings}>
         <div>
-          <img src="http://picsum.photos/400/200" />
+          <img src="http://picsum.photos/id/237/400/200" />
         </div>
         <div>
-          <img src="http://picsum.photos/400/200" />
+          <img src="http://picsum.photos/id/237/400/200" />
         </div>
         <div>
-          <img src="http://picsum.photos/400/200" />
+          <img src="http://picsum.photos/id/237/400/200" />
         </div>
         <div>
-          <img src="http://picsum.photos/400/200" />
+          <img src="http://picsum.photos/id/237/400/200" />
         </div>
       </Slider>
     </div>
