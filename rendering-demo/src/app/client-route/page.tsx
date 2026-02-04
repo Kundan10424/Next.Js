3845b1f322a49587deb0c2494e08f90e4c71fb56
Kundan10424 <kundankumar10424>
2026-02-04 14:39:07 +0530

    server-only code

---
+++ rendering-demo/src/app/client-route/page.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+// import { serverSideFunction } from "@/src/utils/server-utils";
+
+
+export default function ClientRoute(){
+    // const result= serverSideFunction(); // This will cause an error because server-side functions cannot be called from client components.
+    return <h1>Client Route Page</h1>
+}
