c9212e7c0c2a2be5160c5c3668b3479b7b7f383a
Kundan10424 <kundankumar10424>
2026-02-06 16:17:22 +0530

    modified:   src/app/users-server/page.tsx

--- data-fetching/src/app/users-server/page.tsx
+++ data-fetching/src/app/users-server/page.tsx
@@ -13,7 +13,7 @@ export default async function UsersServer(){
     await new Promise((resolve) => {
         setTimeout(resolve, 3000)
     })
-    const response = await fetch("https://jsonplaceholder.typicde.com/users");
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
     const users: User[] = await response.json()
 
     return(
